d1c7dbdde1d5c7180647f9ba871db00a26f18174
shekh khwaja julaha <khwajajulaha122@gmail.com>
2026-05-08 19:50:55 +0530

    8/5/26

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # DSA-C-
+// To make output there are 2 commands in c++
+for minigw 
+---> g++ filename.cpp
+--->./a.exe  
+//endl used for next line........
---
+++ basics/sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{   
+
+    int a,b,c;
+    cout<<"Enter two numbers:"<<endl;
+    cin>>a>>b;
+    c=a+b;
+    cout<<"sum of a and b is c="<<c<<endl;
+    return 0;
+
+}
